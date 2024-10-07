c30d1dde582c75fef4733945989d8915b54ebf3f
Acute-Tri-angle <pptran106@student.hbuhsd.edu>
2024-10-07 11:03:42 -0700

    jisoafhsiodfhsiodfsiodfsdfhsdjhkfjkffjhkfhksdafksjhfkfsjklfsdfjsafsfjhdf safds f sfsa fjhs fjsdahf ffjf fhsdafaf fsjkdf jfhs fdfhsdkfhfjhfsdajfhsjk fhjkdfhsfjd ffhsdajfsa

--- Main.java
+++ Main.java
@@ -4,6 +4,29 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner mlg360doritosUWUemoboy = new Scanner(System.in);
+    Circle a = new Circle(10.1);
+    Circle b = new Circle(14.0);
+    Circle c = new Circle(20.5);
+    System.out.println(a.toString() + "\n" + b.toString() + "\n" + c.toString());
+
+    System.out.println("type # for length + width:");
+    double x = mlg360doritosUWUemoboy.nextDouble();
+    System.out.println("type # for length:");
+    double y = mlg360doritosUWUemoboy.nextDouble();
+    System.out.println("type # for width:");
+    double z = mlg360doritosUWUemoboy.nextDouble();
+
+    Rectangle urmom = new Rectangle(x);
+    Rectangle urdad = new Rectangle(y,z);
+    System.out.println(urmom.toString() + "\n" + urdad.toString());
+
+    System.out.println("type # for side length:");
+    x = mlg360doritosUWUemoboy.nextDouble();
+
+    RegularPolygon urstepsis = new RegularPolygon(x);
+    RegularPolygon urstepbro = new RegularPolygon(4, x);
+    System.out.println(urstepsis.toString() + "\n" + urstepbro.toString());
+    mlg360doritosUWUemoboy.close();
   }
 }
